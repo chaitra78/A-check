149be7c2b3f0ff56825dd402aeb8c3e34f540d8c
chaitra78 <53751398+chaitra78@users.noreply.github.com>
2025-07-07 11:58:13 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,12 +11,4 @@ resource "aws_lambda_function" "appointment-service" {
   role          = devops-role
 }
 
-resource "aws_lambda_function" "patient-service" {
-  function_name = "patient-service"
-  filename      = "${path.module}/patient-service.zip"
-  source_code_hash = filebase64sha256("${path.module}/patient-service.zip")
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  role          = devops-role
-}
 

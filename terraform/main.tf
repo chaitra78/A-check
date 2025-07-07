be4a2a3bd2072947b1b081b09aa628e807b1978b
chaitra78 <53751398+chaitra78@users.noreply.github.com>
2025-07-07 12:27:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,37 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_lambda_function" "appointment-service" {
-  function_name = "appointment-service"
-  filename      = "${path.module}/appointment-service.zip"
-  source_code_hash = filebase64sha256("${path.module}/appointment-service.zip")
-  handler       = "index.handler"
-  runtime       = "nodejs18.x"
-  role          = devops-user
+resource "aws_ecr_repository" "app" {
+  name = "my-microservice"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda-exec-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_basic" {
+  name       = "attach-lambda-basic"
+  roles      = [aws_iam_role.lambda_exec.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "container_lambda" {
+  function_name = "my-app"
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.app.repository_url}:latest"
+  role          = aws_iam_role.lambda_exec.arn
+  timeout       = 10
+  memory_size   = 128
 }
 
 
